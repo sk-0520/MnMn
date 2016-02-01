0d0e52a9ed938353cacc04e8fac8aa39b378454a
sk <content.type.text.net@gmail.com>
2016-02-01 19:41:32 +0900

    不要コメント除外

--- MnMn/MnMn/ViewModel/Window/NicoNico/Video/VideoPlayerViewModel.cs
+++ MnMn/MnMn/ViewModel/Window/NicoNico/Video/VideoPlayerViewModel.cs
@@ -196,62 +196,31 @@ namespace ContentTypeTextNet.MnMn.MnMn.ViewModel.Window.NicoNico.Video
             // TODO: 細かな制御と外部化
             if(RawValueUtility.ConvertBoolean(rawVideoGetflvModel.Done)) {
                 VideoLoadState = LoadState.Loading;
-                
+
                 using(var userAgent = session.CreateHttpUserAgent()) {
-
-                    //var sock = new System.Net.Sockets.Socket(System.Net.Sockets.SocketType.Stream, System.Net.Sockets.ProtocolType.Raw);
-                    //var stream = new NetworkStream(sock);
-                    //var ev = new EventWaitHandle(false, EventResetMode.AutoReset);
-                    //var ip = new IPEndPoint(IPAddress.Any, 12345);
-                    //var sock = new Socket(SocketType.Stream, ProtocolType.IPv4);
-                    //var tcp = new TcpListener(ip);
-
-                    //tcp.Start();
                     var mem = new MemoryStream();
                     var task = Task.Run(async () => {
-                        //ev.Set();
-                       // using(var cl = tcp.AcceptTcpClient()) {
-                        //    cl.SendTimeout = (int)TimeSpan.MaxValue.TotalMilliseconds;
-                            var downloadPath = @"z:\test.mp4";
-                        //    var sss = cl.GetStream();
+                        var downloadPath = @"z:\test.mp4";
 
-                        //var aaa = new byte[8000];
-                        //var read = sss.Read(aaa, 0, aaa.Length);
-                        //var t = Encoding.UTF8.GetString(aaa.Take(read).ToArray());
-                        //userAgent.Timeout = TimeSpan.FromHours(1);
-
-                        //*
                         CanVideoPlay = false;
                         var ss = await userAgent.GetStringAsync(VideoInformationViewModel.WatchUrl);
-                            userAgent.DefaultRequestHeaders.Referrer = VideoInformationViewModel.WatchUrl;
-                        
-                            using(var storageWriter = new BinaryWriter(new FileStream(downloadPath, FileMode.Create, FileAccess.ReadWrite, FileShare.Read))) {
+                        userAgent.DefaultRequestHeaders.Referrer = VideoInformationViewModel.WatchUrl;
+
+                        using(var storageWriter = new BinaryWriter(new FileStream(downloadPath, FileMode.Create, FileAccess.ReadWrite, FileShare.Read))) {
                             // TODO:エコノミー判定
                             VideoSize = VideoInformationViewModel.SizeLow;
-                            
-                                //var rerr = new[] {
-                                //    "HTTP/1.1 200 OK",
-                                //    "Connection: keep-alive",
-                                //    "Content-Type: application/octet-stream",
-                                //    "Content-Type: video/x-msvideo",
-                                //    "Content-Length: " + size,
-                                //};
-                                //var res = string.Join("\r\n", rerr) + "\r\n\r\n";
-                                //var bbb = Encoding.UTF8.GetBytes(res);
-                                //        sss.Write(bbb, 0, (int)bbb.Length);
-                                //        cl.NoDelay = true;
+
                             var idx = 0;
-                            
-                                using(var networkReader = new BinaryReader(await userAgent.GetStreamAsync(rawVideoGetflvModel.MovieServerUrl))) {
+
+                            using(var networkReader = new BinaryReader(await userAgent.GetStreamAsync(rawVideoGetflvModel.MovieServerUrl))) {
                                 byte[] buffer = new byte[Constants.ServiceNicoNicoVideoVideoReceiveBuffer];
-                                    
-                                    int bytesRead=0;
+
+                                int bytesRead = 0;
                                 while(true) {
                                     int n = 0;
                                     int max = 5;
                                     while(true) {
                                         try {
-                                            //Thread.Sleep(1000);
                                             bytesRead = networkReader.Read(buffer, 0, buffer.Length);
                                             break;
                                         } catch(IOException ex) {
@@ -266,149 +235,26 @@ namespace ContentTypeTextNet.MnMn.MnMn.ViewModel.Window.NicoNico.Video
                                     if(bytesRead <= 0) {
                                         break;
                                     }
-                                        storageWriter.Write(buffer, 0, bytesRead);
-                                    //storageWriter.Flush();
-                                        mem.Write(buffer, 0, bytesRead);
-                                        if(!CanVideoPlay) {
-                            var fi = new FileInfo(downloadPath);
+                                    storageWriter.Write(buffer, 0, bytesRead);
+                                    mem.Write(buffer, 0, bytesRead);
+                                    if(!CanVideoPlay) {
+                                        var fi = new FileInfo(downloadPath);
                                         if(fi.Length > videoPlayLowestSize) {
                                             await Task.Run(() => {
-                                            Player.Play(new Uri(downloadPath));
+                                                Player.Play(new Uri(downloadPath));
                                             });
                                             CanVideoPlay = true;
                                         }
                                     }
-                                        //VideoStream.Write(buffer, 0, bytesRead);
-                                        //try {
-                                        //sss.Write(buffer, 0, bytesRead);
-                                        //sss.Flush();
-                                        //var a = Encoding.UTF8.GetBytes(bytesRead.ToString() + "\r\n");
-                                        //var b = Encoding.UTF8.GetBytes("\r\n\r\n");
-                                        //var m = new MemoryStream();
-
-                                        //m.Write(a, 0, a.Length);
-                                        //m.Write(buffer, 0, bytesRead);
-                                        //m.Write(b, 0, b.Length);
-
-                                        //var mb = m.GetBuffer();
-                                        //sss.Write(mb, 0, (int)m.Length);
-
-                                        //sss.Write(buffer, 0, bytesRead);
-                                        VideoLoadedSize += bytesRead;
-                                        Debug.WriteLine("{0}: {1:#,###}  [{2:#,###}/{3:#,###}]", idx++, bytesRead, VideoLoadedSize, VideoSize);
-
-                                        //} catch(IOException ex) {
-                                        //    Debug.WriteLine(ex);
-                                        //}
-                                    }
-                                    //var oooo = Encoding.UTF8.GetBytes("0\r\n\r\n");
-                                    //sss.Write(oooo, 0, oooo.Length);
-
+                                    VideoLoadedSize += bytesRead;
+                                    Debug.WriteLine("{0}: {1:#,###}  [{2:#,###}/{3:#,###}]", idx++, bytesRead, VideoLoadedSize, VideoSize);
                                 }
                             }
-                            //*/
-                            /*
-                            var asd = File.ReadAllBytes(downloadPath);
-                            mem.Write(asd, 0, asd.Length);
-
-                            var size = asd.Length;
-                            var rerr = new[] {
-                                    "HTTP/1.1 200 OK",
-                                    "Connection: keep-alive",
-                                    "Content-Type: application/octet-stream",
-                                    "Content-Type: video/x-msvideo",
-                                    "Content-Length: " + size,
-                                };
-                            var res = string.Join("\r\n", rerr) + "\r\n\r\n";
-                            var bbb = Encoding.UTF8.GetBytes(res);
-                            sss.Write(bbb, 0, (int)bbb.Length);
-
-                            var aaaaaaa = mem.ToArray();
-                            //cl.SendBufferSize=32 * 1024;
-                            var send = cl.SendBufferSize;
-                            var ssss = new BinaryWriter(sss);
-                            int pppppp = 0;
-                            for(var pos = 0; pos < aaaaaaa.Length; pos += send) {
-                                var n = pos + send > aaaaaaa.Length ? aaaaaaa.Length - pos : send;
-                                Debug.WriteLine($"{++pppppp}: {pos}-{n}, / {size}");
-                                var v = 0;
-                                var mx = 4;
-                                while(v++ < mx)
-                                    try {
-                                        ssss.Write(aaaaaaa, pos, n);
-                                        Thread.Sleep(100);
-                                        break;
-                                    } catch(IOException ex) {
-                                        Debug.WriteLine(ex);
-                                        Thread.Sleep(100);
-                                    }
-                            }
-                            sss.Close();
-                            cl.Close();
-                            */
-                    VideoLoadState = LoadState.Loaded;
+                        }
+                        VideoLoadState = LoadState.Loaded;
                         await Task.CompletedTask;
-                        //}
                     });
-                    //ev.WaitOne();
-                    //var options = new[] {
-
-                    //};
-                    //Player.MediaPlayer.Play(new Uri("http://127.0.0.1:12345"), options);
-                    //Player.MediaPlayer.Play(new Uri("http://127.0.0.1:12345"));
-                    //Player.MediaPlayer.Play(new Uri("http://download.blender.org/peach/bigbuckbunny_movies/big_buck_bunny_480p_surround-fix.avi"));
-                    //Player.MediaPlayer.Play(new Uri(@"Z:\test.mp4"));
-
-
-                    /*
-                    var ttt = new TcpClient("127.0.0.1", 12345);
-
-                    var ccc = ttt.GetStream();
-                    ccc.WriteByte(1);
-                    var ddd = new MemoryStream();
-                    var eee = new byte[8*1024];
-                    int fff;
-                    while((fff = ccc.Read(eee, 0, eee.Length)) > 0) {
-                        ddd.Write(eee, 0, fff);
-                    }
                     await task;
-
-                    var pp = new FileStream(@"Z:\@@@.mp4", FileMode.Create);
-                    ddd.Seek(0, SeekOrigin.Begin);
-                    ddd.CopyTo(pp);
-                    pp.Dispose();
-                    //*/
-
-                    await task;
-
-                    //var uri = new Uri(rawVideoGetflvModel.MovieServerUrl);
-                    //var param = new[] {
-                    //    "--imem-cookie=" + session.GetSession(uri)
-                    //};
-                    //Player.MediaPlayer.Playing += MediaPlayer_Playing;
-                    //Player.MediaPlayer.Playing += MediaPlayer_Playing;
-
-                    //Player.MediaPlayer.Play(uri, param);
-
-                    //using(var networkReader = new BinaryReader(await userAgent.GetStreamAsync(rawVideoGetflvModel.MovieServerUrl))) {
-                    //    var downloadPath = @"z:\test.mp4";
-                    //    using(var storageWriter = new BinaryWriter(new FileStream(downloadPath, FileMode.Create, FileAccess.ReadWrite, FileShare.Read))) {
-                    //        VideoStream = new BufferedStream(new FileStream(downloadPath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite));
-                    //        byte[] buffer = new byte[1024];
-                    //        int bytesRead;
-
-                    //        VideoStream = new BufferedStream(new MemoryStream());
-
-                    //        while((bytesRead = networkReader.Read(buffer, 0, 1024)) > 0) {
-                    //            storageWriter.Write(buffer, 0, bytesRead);
-                    //            VideoStream.Write(buffer, 0, bytesRead);
-                    //        }
-                    //        VideoLoadState = LoadState.Loaded;
-
-                    //        Player.MediaPlayer.SetMedia(new FileInfo(downloadPath));
-                    //        Player.MediaPlayer.Play();
-                    //    }
-                    //}
                 }
             }
         }
