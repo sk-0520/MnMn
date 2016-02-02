3351f5df06b5b256d5069efb21c4c5beeffe2db4
sk <content.type.text.net@gmail.com>
2016-02-02 21:10:06 +0900

    うおおおおおお！だめだこりゃぁぁぁぁ

---
+++ MnMn/MnMn/View/Controls/VlcControl.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms.Integration;
+using System.Windows.Media;
+using System.Windows.Threading;
+
+namespace ContentTypeTextNet.MnMn.MnMn.View.Controls
+{
+    public class VlcControl: WindowsFormsHost
+    {
+        public Vlc.DotNet.Forms.VlcControl MediaPlayer { get; private set; }
+
+        public VlcControl()
+        {
+            MediaPlayer = new Vlc.DotNet.Forms.VlcControl();
+            this.Child = MediaPlayer;
+
+        //    IsRedirected = "True"
+        //CompositionMode = "OutputOnly"
+        
+        }
+
+        public bool IsRedirected { get; set; }
+
+    }
+}
--- MnMn/MnMn/ViewModel/Controls/NicoNico/Video/VideoPlayerViewModel.cs
+++ MnMn/MnMn/ViewModel/Controls/NicoNico/Video/VideoPlayerViewModel.cs
@@ -168,6 +168,8 @@ namespace ContentTypeTextNet.MnMn.MnMn.ViewModel.Controls.NicoNico.Video
             set { SetVariableValue(ref this._videoPosition, value); }
         }
 
+        public MediaElement Player2 { get; private set; }
+
         #endregion
 
         #region function
@@ -248,7 +250,7 @@ namespace ContentTypeTextNet.MnMn.MnMn.ViewModel.Controls.NicoNico.Video
             View = view;
             Player = view.player.MediaPlayer;
             VideoSilder = view.videoSilder;
-
+            Player2 = view.mediaElement;
             // あれこれイベント
             View.Closed += View_Closed;
             Player.PositionChanged += Player_PositionChanged;
@@ -258,6 +260,12 @@ namespace ContentTypeTextNet.MnMn.MnMn.ViewModel.Controls.NicoNico.Video
         void AutoPlay(FileInfo fileInfo)
         {
             Player.Play(fileInfo);
+            View.Dispatcher.BeginInvoke(new Action(() => {
+                Player2.LoadedBehavior = MediaState.Manual;
+                Player2.Source = new Uri(VideoPath);
+                Player2.Play();
+            }));
+            //TaskScheduler.FromCurrentSynchronizationContext().
             CanVideoPlay = true;
         }
 
