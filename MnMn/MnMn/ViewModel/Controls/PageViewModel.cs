0b1b861103b86949e80bcc435cbf99103a351f9c
sk <content.type.text.net@gmail.com>
2016-02-25 23:21:47 +0900

    ページャー外部化

---
+++ MnMn/MnMn/ViewModel/Controls/PageViewModel.cs
@@ -0,0 +1,77 @@
+﻿/*
+This file is part of MnMn.
+
+MnMn is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+MnMn is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with MnMn.  If not, see <http://www.gnu.org/licenses/>.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ContentTypeTextNet.Library.SharedLibrary.ViewModel;
+using ContentTypeTextNet.MnMn.MnMn.Define;
+
+namespace ContentTypeTextNet.MnMn.MnMn.ViewModel.Controls
+{
+    public abstract class PageViewModelBase: ViewModelBase
+    {
+        #region variable
+
+        LoadState _loadState;
+
+        #endregion
+
+        #region property
+
+        public virtual int PageNumber { get; }
+
+        public LoadState LoadState
+        {
+            get { return this._loadState; }
+            set { SetVariableValue(ref this._loadState, value); }
+        }
+
+        #endregion
+    }
+
+    public class PageViewModel: PageViewModelBase
+    {
+        public PageViewModel(ViewModelBase viewModelBase)
+        {
+            ViewModelBase = viewModelBase;
+        }
+
+        #region property
+
+        public ViewModelBase ViewModelBase { get; }
+
+        #endregion
+    }
+
+    public class PageViewModel<TViewModel>: PageViewModel
+        where TViewModel : ViewModelBase
+    {
+        public PageViewModel(TViewModel viewModel)
+            : base(viewModel)
+        {
+            ViewModel = viewModel;
+        }
+
+        #region property
+
+        public TViewModel ViewModel { get; }
+
+        #endregion
+    }
+}
