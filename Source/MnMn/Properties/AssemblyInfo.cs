2402b341d582cd9a7aa2c704f36c8f123b798dcd
sk <content.type.text.net@gmail.com>
2016-08-18 21:54:23 +0900

    resolved #40

--- Source/MnMn/Properties/AssemblyInfo.cs
+++ Source/MnMn/Properties/AssemblyInfo.cs
@@ -52,7 +52,7 @@ using Meta.Vlc;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.07.5.*")]
+[assembly: AssemblyVersion("0.07.6.*")]
 [assembly: AssemblyInformationalVersion("xxxxxxxxxxxxxxDEVELOPMENTxxxxxxxxxxxxxxx")]
 
 [assembly: VlcSettings(@"lib\LibVlc")]
