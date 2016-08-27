be607aad4aec6c8503167d444cf54dd10d151d12
sk <content.type.text.net@gmail.com>
2016-08-28 02:31:15 +0900

    次回リリースを表示してみた

--- Source/MnMn/ViewModel/Controls/App/AppIssueManagerViewModel.cs
+++ Source/MnMn/ViewModel/Controls/App/AppIssueManagerViewModel.cs
@@ -16,34 +16,39 @@ along with MnMn.  If not, see <http://www.gnu.org/licenses/>.
 */
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
-using System.Windows.Input;
 using ContentTypeTextNet.MnMn.MnMn.Define;
 using ContentTypeTextNet.MnMn.MnMn.Logic;
 using ContentTypeTextNet.MnMn.MnMn.View.Controls;
 
 namespace ContentTypeTextNet.MnMn.MnMn.ViewModel.Controls.App
 {
-    public class AppHelpManagerViewModel: ManagerViewModelBase
+    public class AppIssueManagerViewModel: ManagerViewModelBase
     {
-        public AppHelpManagerViewModel(Mediation mediation)
+        public AppIssueManagerViewModel(Mediation mediation)
             : base(mediation)
         { }
 
         #region property
 
-        WebBrowser HelpBrowser { get; set; }
+        WebBrowser IssueBrowser { get; set; }
 
         #endregion
 
-        #region command
-        #endregion
+        #region ManagerViewModelBas
 
-        #region ManagerViewModelBase
+        protected override void ShowView()
+        {
+            base.ShowView();
+
+            if(IssueBrowser.Source == null) {
+                IssueBrowser.Navigate(Constants.AppUriIssueResolved);
+            }
+        }
 
         public override Task<long> GarbageCollectionAsync(GarbageCollectionLevel garbageCollectionLevel, CacheSpan cacheSpan)
         {
@@ -57,20 +62,12 @@ namespace ContentTypeTextNet.MnMn.MnMn.ViewModel.Controls.App
 
         public override void InitializeView(MainWindow view)
         {
-            HelpBrowser = view.information.helpBrowser;
+            IssueBrowser = view.information.issueBrowser;
         }
 
         public override void UninitializeView(MainWindow view)
         {
-        }
-
-        protected override void ShowView()
-        {
-            base.ShowView();
-
-            if(HelpBrowser.Source == null) {
-                HelpBrowser.Source = new Uri(Constants.HelpFilePath);
-            }
+            IssueBrowser = null;
         }
 
         #endregion
