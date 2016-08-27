30ce914493a8d98fa67910e53dbc8e95bd688775
sk <content.type.text.net@gmail.com>
2016-08-27 20:47:09 +0900

    WindowChrome.CaptionHeightが泥臭い

---
+++ Source/MnMn/View/Converter/Service/Smile/Video/CaptionHeightConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace ContentTypeTextNet.MnMn.MnMn.View.Converter.Service.Smile.Video
+{
+    public class CaptionHeightConverter: IMultiValueConverter
+    {
+        #region IMultiValueConverter
+
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            var isNormalWindow = (bool)values[0];
+            var realHeight = (double)values[1];
+
+            if(isNormalWindow) {
+                return realHeight;
+            } else {
+                return (double)parameter;
+            }
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+
+        #endregion
+    }
+}
