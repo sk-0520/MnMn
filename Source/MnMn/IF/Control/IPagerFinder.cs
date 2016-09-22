c2da6fd49e0ccd228f1655f4cb41029522a3ca4c
sk <content.type.text.net@gmail.com>
2016-09-22 21:41:40 +0900

    IPagerFinder

---
+++ Source/MnMn/IF/Control/IPagerFinder.cs
@@ -0,0 +1,48 @@
+﻿/*
+This file is part of MnMn.
+
+MnMn is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+MnMn is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with MnMn.  If not, see <http://www.gnu.org/licenses/>.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ContentTypeTextNet.Library.SharedLibrary.Model;
+using ContentTypeTextNet.MnMn.MnMn.ViewModel.Controls;
+
+namespace ContentTypeTextNet.MnMn.MnMn.IF.Control
+{
+    /// <summary>
+    /// ページャを保持するFinder。
+    /// </summary>
+    public interface IPagerFinder<TFinderViewModel, TInformationViewModel, TFinderItemViewModel>
+        where TFinderViewModel : FinderViewModelBase<TInformationViewModel, TFinderItemViewModel>
+        where TInformationViewModel : InformationViewModelBase
+        where TFinderItemViewModel : FinderItemViewModelBase<TInformationViewModel>
+    {
+        #region property
+
+        /// <summary>
+        /// 保持するページャ。
+        /// </summary>
+        CollectionModel<PageViewModel<TFinderViewModel>> PageItems { get; }
+        /// <summary>
+        /// 選択中ページ。
+        /// </summary>
+        PageViewModel<TFinderViewModel> SelectedPage { get; set; }
+
+        #endregion
+    }
+}
