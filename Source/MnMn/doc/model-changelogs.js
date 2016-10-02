ace28e1e90598313e8f7d70ac8a252c559f38dc6
sk <content.type.text.net@gmail.com>
2016-10-02 21:11:54 +0900

    init 0.24.0

--- Source/MnMn/doc/model-changelogs.js
+++ Source/MnMn/doc/model-changelogs.js
@@ -84,6 +84,80 @@
 	},
 	*/
 	{
+		'date': 'YYYY/MM/DD',
+		'version': '0.23.1',
+		'isRc': true,
+		'contents': [
+			{
+				'type': 'note',
+				'logs': [
+					{
+						'revision': '',
+						'subject': ''
+					},
+					{
+						'revision': '',
+						'subject': ''
+					},
+					{
+						'revision': '',
+						'subject': ''
+					}
+				]
+			},
+			{
+				'type': 'features',
+				'logs': [
+					{
+						'revision': '',
+						'subject': ''
+					},
+					{
+						'revision': '',
+						'subject': ''
+					},
+					{
+						'revision': '',
+						'subject': ''
+					}
+				]
+			},
+			{
+				'type': 'fixes',
+				'logs': [
+					{
+						'revision': '',
+						'subject': ''
+					},
+					{
+						'revision': '',
+						'subject': ''
+					},
+					{
+						'revision': '',
+						'subject': ''
+					}
+				]
+			},
+			{
+				'type': 'developer',
+				'logs': [
+					{
+						'revision': '',
+						'subject': ''
+					},
+					{
+						'revision': '',
+						'subject': ''
+					},
+					{
+						'revision': '',
+						'subject': ''
+					}
+				]
+			}
+		]
+	}, {
 		'date': '2016/10/02',
 		'version': '0.23.0',
 		'isRc': false,
