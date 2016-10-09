a393b48a49b0557bd7c83f715941a73c9d324d08
sk <content.type.text.net@gmail.com>
2016-10-09 16:50:16 +0900

    mylist

--- Source/MnMn/View/Converter/Service/Smile/Video/MyLIstFolderIdToColorConverter.cs
+++ Source/MnMn/View/Converter/Service/Smile/Video/MyLIstFolderIdToColorConverter.cs
@@ -32,7 +32,7 @@ using ContentTypeTextNet.MnMn.MnMn.Model.Service.Smile.Video;
 
 namespace ContentTypeTextNet.MnMn.MnMn.View.Converter.Service.Smile.Video
 {
-    [ValueConversion(typeof(DefinedElementModel), typeof(BitmapSource))]
+    [ValueConversion(typeof(DefinedElementModel), typeof(Color))]
     public class MyListFolderIdToColorConverter: IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
@@ -40,9 +40,10 @@ namespace ContentTypeTextNet.MnMn.MnMn.View.Converter.Service.Smile.Video
             var element = value as DefinedElementModel;
             if(element != null) {
                 var color = SmileMyListUtility.GetColorsFromExtends(element.Extends).First();
-                var image = BitmapFrame.Create(SharedConstants.GetPackUri((string)parameter));
-                FreezableUtility.SafeFreeze(image);
-                return MediaUtility.ColoringImage(image, color);
+                //var image = BitmapFrame.Create(SharedConstants.GetPackUri((string)parameter));
+                //FreezableUtility.SafeFreeze(image);
+                //return MediaUtility.ColoringImage(image, color);
+                return color;
             }
 
             return null;
