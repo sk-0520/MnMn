793cdc248cf7712fce4e55cc8c36f69302a48544
sk <content.type.text.net@gmail.com>
2016-10-24 22:06:45 +0900

    コメント

--- Source/MnMn/Model/Setting/FilteringItemSettingModel.cs
+++ Source/MnMn/Model/Setting/FilteringItemSettingModel.cs
@@ -29,6 +29,9 @@ namespace ContentTypeTextNet.MnMn.MnMn.Model.Setting
     {
         #region property
 
+        /// <summary>
+        /// 有効無効。
+        /// </summary>
         [DataMember]
         public bool IsEnabled { get; set; } = true;
 
