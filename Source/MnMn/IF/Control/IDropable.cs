29d7ffbf8c152d3cab2000eebb60d4f2c89f0cdf
sk <content.type.text.net@gmail.com>
2016-11-22 22:28:27 +0900

    D&D用IF追加

---
+++ Source/MnMn/IF/Control/IDropable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContentTypeTextNet.MnMn.MnMn.IF.Control
+{
+    public interface IDropable
+    {
+        /// <summary>
+        /// ドラッグ中アイテムが上に存在しているか。
+        /// </summary>
+        bool IsDragOver { get; set; }
+    }
+}
