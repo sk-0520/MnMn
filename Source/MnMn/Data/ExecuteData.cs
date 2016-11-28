f97fc27e3b0e2d468f66a3d4a19b7e6bad47c573
sk <content.type.text.net@gmail.com>
2016-11-28 22:57:53 +0900

    レジストリからあれこれブラウザっぽいのを捕まえてくる

---
+++ Source/MnMn/Data/ExecuteData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContentTypeTextNet.MnMn.MnMn.Data
+{
+    public struct ExecuteData
+    {
+        public ExecuteData(string applicationPath, IEnumerable<string> arguments)
+        {
+            ApplicationPath = applicationPath;
+
+            if(arguments == null) {
+                Arguments = new List<string>();
+            } else {
+                Arguments = arguments.ToList();
+            }
+        }
+
+        #region property
+
+        public string ApplicationPath { get; }
+
+        public IReadOnlyList<string> Arguments { get; }
+
+        #endregion
+    }
+}
--- Source/MnMn/Logic/WebNavigatorCore.cs
+++ Source/MnMn/Logic/WebNavigatorCore.cs
@@ -16,12 +16,14 @@ along with MnMn.  If not, see <http://www.gnu.org/licenses/>.
 */
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using ContentTypeTextNet.MnMn.MnMn.Data;
 using ContentTypeTextNet.MnMn.MnMn.Define;
 using ContentTypeTextNet.MnMn.MnMn.Logic.Extensions;
 using ContentTypeTextNet.MnMn.MnMn.Logic.Utility;
@@ -32,6 +34,7 @@ using ContentTypeTextNet.MnMn.MnMn.ViewModel;
 using ContentTypeTextNet.Pe.PeMain.Logic.Utility;
 using Gecko;
 using Gecko.Cache;
+using Microsoft.Win32;
 
 namespace ContentTypeTextNet.MnMn.MnMn.Logic
 {
@@ -85,6 +88,10 @@ namespace ContentTypeTextNet.MnMn.MnMn.Logic
                     break;
             }
 
+            var command = GetDefaultBrowserCommand();
+            var pureExecData = GetBrowserPureExecuteData(command);
+            var data = GetBrowserExecuteData(pureExecData, new Uri("http://asd.cxz"));
+
             IsInitialized = true;
             IsUninitialized = false;
         }
@@ -132,6 +139,89 @@ namespace ContentTypeTextNet.MnMn.MnMn.Logic
                 session.ApplyToWebNavigatorEngine(Engine, uri);
             });
         }
+
+        static string GetDefaultBrowserCommandCore(RegistryKey registryKey, string name)
+        {
+            using(var registry = registryKey.OpenSubKey(name, false)) {
+                if(registry == null) {
+                    return null;
+                }
+
+                var value = (string)registry.GetValue(null);
+                return value;
+            }
+        }
+
+        public static string GetDefaultBrowserCommand()
+        {
+            var currentUser = GetDefaultBrowserCommandCore(Registry.CurrentUser, @"Software\Microsoft\Windows\Shell\Associations\UrlAssociations\http");
+            if(currentUser != null) {
+                return currentUser;
+            }
+
+            var classesRoot = GetDefaultBrowserCommandCore(Registry.ClassesRoot, @"HTTP\shell\open\command");
+            return classesRoot;
+        }
+
+        static ExecuteData GetBrowserPureExecuteDataCore(string browserCommand)
+        {
+            Debug.Assert(browserCommand != null);
+
+            string applicationPath;
+            int applicationSkipIndex;
+
+            if(browserCommand.First() == '"') {
+                // "xxx.exe" nn nnn 形式
+                var basePath = browserCommand
+                    .Skip(1)
+                    .TakeWhile(c => c != '"')
+                    .ToArray()
+                ;
+                applicationPath = new string(basePath);
+                applicationSkipIndex = 1;
+            } else {
+                // xxx.exe nn nnn 形式
+                var ext = ".exe";
+                var index = browserCommand.IndexOf(ext, StringComparison.OrdinalIgnoreCase);
+                applicationPath = browserCommand.Substring(0, index + ext.Length);
+                applicationSkipIndex = 0;
+            }
+
+            var baseArguments = browserCommand
+                .Skip(applicationSkipIndex)
+                .Skip(applicationPath.Length)
+                .Skip(applicationSkipIndex)
+                .ToArray()
+            ;
+            var argument = new string(baseArguments);
+
+            return new ExecuteData(applicationPath, argument.Split(' ').Where(s => !string.IsNullOrWhiteSpace(s)));
+        }
+
+        public static ExecuteData GetBrowserPureExecuteData(string browserCommand)
+        {
+            if(browserCommand == null) {
+                throw new ArgumentNullException(nameof(browserCommand));
+            }
+
+            return GetBrowserPureExecuteDataCore(browserCommand);
+        }
+
+        public static ExecuteData GetBrowserExecuteData(ExecuteData executeData, Uri uri)
+        {
+            IEnumerable<string> arguments;
+
+            if(executeData.Arguments.Any(s => s.IndexOf("%1") != -1)) {
+                arguments = executeData.Arguments.Select(s => s.Replace("%1", uri.OriginalString));
+            } else {
+                var list = executeData.Arguments.ToList();
+                list.Add(uri.OriginalString);
+                arguments = list;
+            }
+
+            return new ExecuteData(executeData.ApplicationPath, arguments);
+        }
+
 
         #endregion
     }
