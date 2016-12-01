3c5475cc6186cbfe0a0c9602eb983c20b50c526a
sk <content.type.text.net@gmail.com>
2016-12-01 20:43:33 +0900

    0.33.0

--- Source/MnMn/Properties/AssemblyInfo.cs
+++ Source/MnMn/Properties/AssemblyInfo.cs
@@ -15,7 +15,7 @@ using Meta.Vlc;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.32.6.*")]
+[assembly: AssemblyVersion("0.33.0.*")]
 [assembly: AssemblyInformationalVersion("xxxxxxxxxxxxxxDEVELOPMENTxxxxxxxxxxxxxxx")]
 
 #region あまり変わらない部分
