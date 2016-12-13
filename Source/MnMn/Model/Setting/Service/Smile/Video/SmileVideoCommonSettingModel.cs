f95293e5cbafdd3d5b04c3bba1681c450b5ed3f1
sk <content.type.text.net@gmail.com>
2016-12-13 22:53:09 +0900

    カスタムコピー用フォーマット追加

---
+++ Source/MnMn/Model/Setting/Service/Smile/Video/SmileVideoCommonSettingModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContentTypeTextNet.MnMn.MnMn.Model.Setting.Service.Smile.Video
+{
+    [DataContract]
+    public class SmileVideoCommonSettingModel: SettingModelBase
+    {
+        #region property
+
+        [DataMember]
+        public string CustomCopyFormat { get; set; }
+
+        #endregion
+    }
+}
