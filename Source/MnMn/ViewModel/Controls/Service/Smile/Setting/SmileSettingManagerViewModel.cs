6e3a993829199e82b74d5ce6530248cdd0a1aef2
sk <content.type.text.net@gmail.com>
2016-12-13 23:06:19 +0900

    設定UIを追加
    各変数は未実装

--- Source/MnMn/ViewModel/Controls/Service/Smile/Setting/SmileSettingManagerViewModel.cs
+++ Source/MnMn/ViewModel/Controls/Service/Smile/Setting/SmileSettingManagerViewModel.cs
@@ -76,6 +76,25 @@ namespace ContentTypeTextNet.MnMn.MnMn.ViewModel.Controls.Service.Smile.Setting
             set { SetVariableValue(ref this._editingAccountPassword, value); }
         }
 
+        public string CommonCustomCopyFormat
+        {
+            get { return Setting.Video.Common.CustomCopyFormat; }
+            set { SetPropertyValue(Setting.Video.Common, value, nameof(Setting.Video.Common.CustomCopyFormat)); }
+        }
+
+        public string CommonCustomCopyList
+        {
+            get
+            {
+                var list = new[] {
+                    "a",
+                    "b",
+                };
+
+                return string.Join(", ", list.Select(s => "${" + s + "}"));
+            }
+        }
+
         /// <summary>
         /// 動画再生方法。
         /// </summary>
