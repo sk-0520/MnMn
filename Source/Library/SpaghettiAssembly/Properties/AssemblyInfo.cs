4a4164ab2fecf6048e645f5496902c73d6ec3bf0
sk <content.type.text.net@gmail.com>
2017-01-01 03:22:03 +0900

    破棄可能用アセンブリ作成

--- Source/Library/SpaghettiAssembly/Properties/AssemblyInfo.cs
+++ Source/Library/SpaghettiAssembly/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("Package")]
+[assembly: AssemblyTitle("SpaghettiAssembly")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Package")]
-[assembly: AssemblyCopyright("Copyright © 2016-<YEAR> sk <content-type-text.net>")]
+[assembly: AssemblyProduct("SpaghettiAssembly")]
+[assembly: AssemblyCopyright("Copyright © 2017-<YEAR> sk <content-type-text.net>")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
-[assembly: Guid("922d77b4-f3b7-4685-b6dc-6ecb0f4bbe8f")]
+[assembly: Guid("cde064a0-0133-4c6b-ad46-32e9429d4715")]
 
 // アセンブリのバージョン情報は次の 4 つの値で構成されています:
 //
@@ -32,5 +32,4 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
