9bcfa783ddae1bdab664b5330f9c6982c9d402c4
sk <content.type.text.net@gmail.com>
2017-01-05 19:31:00 +0900

    まずXAMLのキーを記号定数化

--- Source/MnMn/ViewModel/Controls/Service/Smile/Video/Player/SmileVideoPlayerViewModel.cs
+++ Source/MnMn/ViewModel/Controls/Service/Smile/Video/Player/SmileVideoPlayerViewModel.cs
@@ -711,7 +711,7 @@ namespace ContentTypeTextNet.MnMn.MnMn.ViewModel.Controls.Service.Smile.Video.Pl
                 // リスト部は比率レイアウトなので補正が必要
 
                 // TODO: うーん、ださい
-                var defaultGridSplitterLength = (double)Application.Current.Resources["GridSplitterLength"];
+                var defaultGridSplitterLength = (double)Application.Current.Resources[Constants.xamlGridSplitterLength];
 
                 leaveSize.Width = videoSize.Width / PlayerAreaLength.Value.Value * CommentAreaLength.Value.Value;
             }
