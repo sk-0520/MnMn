df7078e991dc368579a1a9c294dcbde0fbae8aa5
sk <content.type.text.net@gmail.com>
2017-01-08 10:16:59 +0900

    パス追加

--- Source/Setup/Constants.cs
+++ Source/Setup/Constants.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,25 +11,55 @@ namespace ContentTypeTextNet.MnMn.Setup
     {
         #region variable
 
+        const string projectName = "MnMn";
+
         const string mutexName = "MnMn";
 
         #endregion
 
         #region property
 
+        public static string MutexName
+        {
+            get
+            {
 #if DEBUG
-        /// <summary>
-        /// アプリケーション使用名。
-        /// </summary>
-        public static string MutexName { get; } = mutexName + "-debug";
+                return mutexName + "-debug";
 #else
-        /// <summary>
-        /// アプリケーション使用名。
-        /// </summary>
-        public static string MutexName { get; } = mutexName;
+                return mutexName;
 #endif
+            }
+        }
 
         public static TimeSpan MutexWaitTime { get; } = TimeSpan.FromSeconds(3);
+
+        public static Uri UpdateUri
+        {
+            get
+            {
+#if DEBUG
+                return new Uri("http://localhost/test/mnmn/update.xml");
+#else
+                return new Uri("https://bitbucket.org/sk_0520/mnmn/downloads/update.xml");
+#endif
+            }
+        }
+
+        public static string SettingBaseDirectoryPath
+        {
+            get
+            {
+                var basePath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+#if DEBUG
+                return Path.Combine(basePath, projectName + "-debug");
+#else
+                return Path.Combine(basePath, projectName);
+#endif
+            }
+        }
+
+        public static string InstallDirectoryPath { get; } = Path.Combine(SettingBaseDirectoryPath, "application");
+        public static string ArchiveDirectoryPath { get; } = Path.Combine(SettingBaseDirectoryPath, "archive");
 
         #endregion
     }
