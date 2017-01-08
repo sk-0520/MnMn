54854dfaae13c53920060a8c39fd5f98d1421ccb
sk <content.type.text.net@gmail.com>
2017-01-08 09:11:00 +0900

    名前空間変更

--- Source/Setup/App.xaml.cs
+++ Source/Setup/App.xaml.cs
@@ -6,12 +6,22 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace MnMn_Setup
+namespace ContentTypeTextNet.MnMn.Setup
 {
     /// <summary>
     /// App.xaml の相互作用ロジック
     /// </summary>
     public partial class App: Application
     {
+        #region Application
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+
+        }
+
+        #endregion
     }
 }
--- Source/Setup/MainWindow.xaml.cs
+++ Source/Setup/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace MnMn_Setup
+namespace ContentTypeTextNet.MnMn.Setup
 {
     /// <summary>
     /// MainWindow.xaml の相互作用ロジック
