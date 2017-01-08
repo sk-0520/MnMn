da545e4da67d8963b558e3aa5308dec1afb360ef
sk <content.type.text.net@gmail.com>
2017-01-08 12:27:50 +0900

    (´・ω・`)

--- Source/Setup/MainWindow.xaml.cs
+++ Source/Setup/MainWindow.xaml.cs
@@ -24,5 +24,60 @@ namespace ContentTypeTextNet.MnMn.Setup
         {
             InitializeComponent();
         }
+
+        #region function
+
+        void ResetInstallDirectoryPath()
+        {
+            this.inputInstallDirectoryPath.Text = Constants.InstallDirectoryPath;
+        }
+
+        void AddLog(LogItem logItem)
+        {
+            this.listLog.Items.Add(logItem);
+        }
+
+        Task InstallCoreAsync(string installPath, bool createShortcut, bool installToExecute)
+        {
+            return Task.CompletedTask;
+        }
+
+        Task InstallAsync()
+        {
+            var installPath = inputInstallDirectoryPath.Text;
+            var createShortcut = selectCreateShortcut.IsChecked.GetValueOrDefault();
+            var installToExecute = selectInstallToExecute.IsChecked.GetValueOrDefault();
+
+            AddLog(new LogItem(LogKind.Message, "start"));
+
+            IsEnabled = false;
+            return InstallCoreAsync(installPath, createShortcut, installToExecute).ContinueWith(_ => {
+                IsEnabled = true;
+
+                AddLog(new LogItem(LogKind.Message, "end"));
+            }, TaskScheduler.FromCurrentSynchronizationContext());
+        }
+
+        #endregion
+
+        private void Window_Initialized(object sender, EventArgs e)
+        {
+            ResetInstallDirectoryPath();
+        }
+
+        private void commandResetInstallDirectoryPath_Click(object sender, RoutedEventArgs e)
+        {
+            ResetInstallDirectoryPath();
+        }
+
+        private void commandInstall_Click(object sender, RoutedEventArgs e)
+        {
+            InstallAsync();
+        }
+
+        private void commandClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
     }
 }
