dc4c14c6a9cc91e39f8c36109cb1e606c27f126f
sk <content.type.text.net@gmail.com>
2017-01-08 09:31:56 +0900

    mutex を MnMn からパクる

--- Source/Setup/App.xaml.cs
+++ Source/Setup/App.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,13 +14,31 @@ namespace ContentTypeTextNet.MnMn.Setup
     /// </summary>
     public partial class App: Application
     {
+        #region property
+
+        Mutex Mutex { get; } = new Mutex(false, Constants.MutexName);
+
+        #endregion
+
         #region Application
 
         protected override void OnStartup(StartupEventArgs e)
         {
+            if(!Mutex.WaitOne(Constants.MutexWaitTime, false)) {
+                Shutdown();
+                return;
+            }
+
             base.OnStartup(e);
+        }
 
+        protected override void OnExit(ExitEventArgs e)
+        {
+            if(Mutex != null) {
+                Mutex.Dispose();
+            }
 
+            base.OnExit(e);
         }
 
         #endregion
---
+++ Source/Setup/Constants.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContentTypeTextNet.MnMn.Setup
+{
+    public static class Constants
+    {
+        #region variable
+
+        const string mutexName = "MnMn";
+
+        #endregion
+
+        #region property
+
+#if DEBUG
+        /// <summary>
+        /// アプリケーション使用名。
+        /// </summary>
+        public static string MutexName { get; } = mutexName + "-debug";
+#else
+        /// <summary>
+        /// アプリケーション使用名。
+        /// </summary>
+        public static string MutexName { get; } = mutexName;
+#endif
+
+        public static TimeSpan MutexWaitTime { get; } = TimeSpan.FromSeconds(3);
+
+        #endregion
+    }
+}
