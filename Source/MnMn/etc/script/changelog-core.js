e8981c9245e355bf73d76a6e7085b421e0690ca4
sk <content.type.text.net@gmail.com>
2017-01-21 20:36:24 +0900

    共通は無理だからインラインというか埋め込み的な要素は静的生成に任せることにした

--- Source/MnMn/etc/script/changelog-core.js
+++ Source/MnMn/etc/script/changelog-core.js
@@ -174,26 +174,8 @@ function CreateImageElement(text, image, isEmbeddedImage, imageBaseDirPath)
 	var img = new Element('img');
 	img.setAttibute('alt', text);
 	if (isEmbeddedImage) {
-		var ext = image.split('.').pop().toLowerCase();
-		var map = {
-			'jpg': 'jpeg',
-			'jpeg': 'jpeg',
-			'png': 'png'
-		};
-		var srcHead = 'data:image/' + map[ext] + ';base64,';
-		var srcBase64 = '';
-
-		var stream = WScript.CreateObject('ADODB.Stream');
-		stream.Mode = 3;
-		stream.Type = 2;
-		stream.Charset = 'UTF-8';
-		stream.Open();
-		stream.LoadFromFile(imageBaseDirPath + "\\" + image);
-		var binary = stream.Read();
-
-		var src = srcHead + srcBase64;
-
-		img.setAttibute('src', src);
+		img.setAttibute('src', imageBaseDirPath + '\\' + image);
+		img.setAttibute('class', 'embedded')
 	} else {
 		img.setAttibute('src', imageBaseDirPath + '/' + image);
 	}
