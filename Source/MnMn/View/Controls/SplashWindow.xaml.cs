60d083eea3c2e6b30fddfa67c56bcd3aa866e1d5
sk <content.type.text.net@gmail.com>
2017-02-04 22:21:34 +0900

    Alt+F4の無効化

--- Source/MnMn/View/Controls/SplashWindow.xaml.cs
+++ Source/MnMn/View/Controls/SplashWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
@@ -19,9 +20,38 @@ namespace ContentTypeTextNet.MnMn.MnMn.View.Controls
     /// </summary>
     public partial class SplashWindow: Window
     {
+        #region define
+
+        const int WM_SYSKEYDOWN = 0x0104;
+        const int VK_F4 = 0x73;
+
+        #endregion
+
         public SplashWindow()
         {
             InitializeComponent();
+        }
+
+        #region function
+
+        IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            if((msg == WM_SYSKEYDOWN) && (wParam.ToInt32() == VK_F4)) {
+                handled = true;
+            }
+
+            return IntPtr.Zero;
+        }
+
+        #endregion
+
+        #region Window
+        #endregion
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var source = HwndSource.FromHwnd(new WindowInteropHelper(this).Handle);
+            source.AddHook(new HwndSourceHook(WndProc));
         }
     }
 }
