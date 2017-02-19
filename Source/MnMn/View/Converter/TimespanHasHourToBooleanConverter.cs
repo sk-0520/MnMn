6fa68be6713b2d2e5173c809b03dd454d0be0c08
sk <content.type.text.net@gmail.com>
2017-02-19 14:48:54 +0900

    resolved #409

---
+++ Source/MnMn/View/Converter/TimespanHasHourToBooleanConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace ContentTypeTextNet.MnMn.MnMn.View.Converter
+{
+    [ValueConversion(typeof(TimeSpan), typeof(bool))]
+    public class TimespanHasHourToBooleanConverter: IValueConverter
+    {
+        #region IValueConverter
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var time = (TimeSpan)value;
+            return 1 <= time.TotalHours;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+
+        #endregion
+    }
+}
