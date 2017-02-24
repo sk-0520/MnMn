0ea84db27a25a25aae697e9c56db8e59a351acfb
sk <content.type.text.net@gmail.com>
2017-02-24 22:10:51 +0900

    MnMn と アセンブリ共有

--- Source/Applications/bin/CrashSender/App.xaml.cs
+++ Source/Applications/bin/CrashSender/App.xaml.cs
@@ -2,16 +2,62 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace CrashSender
+namespace ContentTypeTextNet.MnMn.Applications.CrashSender
 {
     /// <summary>
     /// App.xaml の相互作用ロジック
     /// </summary>
     public partial class App: Application
     {
+        public App()
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
+        }
+
+        #region Application
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var viewModel = new MainWorkerViewModel();
+            var mainWindow = new MainWindow() {
+                DataContext = viewModel,
+            };
+            viewModel.SetView(mainWindow);
+
+            MainWindow = mainWindow;
+            MainWindow.Show();
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            AppDomain.CurrentDomain.AssemblyResolve -= CurrentDomain_AssemblyResolve;
+
+            base.OnExit(e);
+        }
+
+        #endregion
+
+        private Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            var name = new AssemblyName(args.Name).Name + ".dll";
+            var path = Path.Combine(Constants.LibraryDirectoryPath, name);
+            var absPath = Path.GetFullPath(path);
+            if(File.Exists(absPath)) {
+                var asm = Assembly.LoadFrom(absPath);
+                return asm;
+            }
+
+            // 見つかんないともう何もかもおかしい、と思ったけど resource.dll もこれで飛んでくんのかい
+            //throw new FileNotFoundException(absPath);
+            return null;
+        }
     }
 }
--- Source/Applications/bin/CrashSender/MainWindow.xaml.cs
+++ Source/Applications/bin/CrashSender/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace CrashSender
+namespace ContentTypeTextNet.MnMn.Applications.CrashSender
 {
     /// <summary>
     /// MainWindow.xaml の相互作用ロジック
