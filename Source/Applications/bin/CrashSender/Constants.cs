70f412299b252b6f53c76cb81473fcc9b05b292d
sk <content.type.text.net@gmail.com>
2017-02-25 15:23:17 +0900

    おおう、追加忘れ

---
+++ Source/Applications/bin/CrashSender/Constants.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContentTypeTextNet.MnMn.Applications.CrashSender
+{
+    public static class Constants
+    {
+        #region property
+
+        /// <summary>
+        /// アプリケーションパス。
+        /// </summary>
+        public static string AssemblyPath { get; } = Assembly.GetExecutingAssembly().Location;
+        /// <summary>
+        /// アプリケーション親ディレクトリパス。
+        /// </summary>
+        public static string AssemblyRootDirectoryPath { get; } = Path.GetDirectoryName(AssemblyPath);
+
+        /// <summary>
+        /// ライブラリ周りを MnMn と共有。
+        /// </summary>
+        public static string LibraryDirectoryPath { get; } = Path.Combine(AssemblyRootDirectoryPath, "..", "..", "lib");
+
+        #endregion
+    }
+}
