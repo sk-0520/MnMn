b669e97f7123e4c4b7f967d1d5ef888202e498d0
sk <content.type.text.net@gmail.com>
2017-03-01 19:16:16 +0900

    ログ取得はProcessで実施するようにした

---
+++ Source/MnMn/Model/Request/Parameter/AppLoggingParameterModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ContentTypeTextNet.Library.SharedLibrary.Model;
+
+namespace ContentTypeTextNet.MnMn.MnMn.Model.Request.Parameter
+{
+    public class AppLoggingParameterModel: ProcessParameterModelBase
+    {
+        #region property
+
+        /// <summary>
+        /// 複製したログを取得するか。
+        /// </summary>
+        public bool GetClone { get; set; }
+
+        #endregion
+    }
+}
