bfc1f7a532b650ba1b37bc6eae3204278cdfdb69
sk <content.type.text.net@gmail.com>
2017-03-08 22:48:49 +0900

    init 0.54.0

--- Source/MnMn/Properties/AssemblyInfo.cs
+++ Source/MnMn/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 using System.Windows;
 using Meta.Vlc;
 
-[assembly: AssemblyVersion("0.53.0.*")]
+[assembly: AssemblyVersion("0.53.1.*")]
 
 #region あまり変わらない部分
 
