699069348a665019506d984e22dfd4dca04afe41
sk <content.type.text.net@gmail.com>
2017-03-11 12:00:04 +0900

    #456 doc

--- Source/MnMn/Properties/AssemblyInfo.cs
+++ Source/MnMn/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 using System.Windows;
 using Meta.Vlc;
 
-[assembly: AssemblyVersion("0.54.1.*")]
+[assembly: AssemblyVersion("0.54.2.*")]
 
 #region あまり変わらない部分
 
