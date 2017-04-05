8e24e72a57a068c44a500280579c0f9d75acc5ba
sk <content.type.text.net@gmail.com>
2017-04-05 20:29:08 +0900

    resolved #514

--- Source/MnMn/Properties/AssemblyInfo.cs
+++ Source/MnMn/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 using System.Windows;
 using Meta.Vlc;
 
-[assembly: AssemblyVersion("0.59.0.*")]
+[assembly: AssemblyVersion("0.59.1.*")]
 
 #region あまり変わらない部分
 
