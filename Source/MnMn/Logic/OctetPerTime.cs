ad8ff3fe84a84b1c50f4c063abc35656565c5acb
sk <content.type.text.net@gmail.com>
2017-04-16 01:21:56 +0900

    ダウンローダーから見様見真似でこぴった

---
+++ Source/MnMn/Logic/OctetPerTime.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContentTypeTextNet.MnMn.MnMn.Logic
+{
+    public class OctetPerTime
+    {
+        public OctetPerTime(TimeSpan baseTime)
+        {
+            BaseTime = baseTime;
+        }
+
+        #region property
+
+        public TimeSpan BaseTime { get; }
+
+        Stopwatch TimeStopWatch { get; } = new Stopwatch();
+
+        protected long SizeInBaseTime { get; private set; }
+        protected long PrevSize { get; private set; }
+        public long Size { get; protected set; }
+
+        #endregion
+
+        #region function
+
+        public void Start()
+        {
+            TimeStopWatch.Restart();
+            SizeInBaseTime = 0;
+        }
+
+        public void Add(long addSize)
+        {
+            var elapsedTime = TimeStopWatch.Elapsed;
+
+            if(BaseTime <= elapsedTime) {
+                Size = SizeInBaseTime + addSize;
+
+                PrevSize = Size;
+                SizeInBaseTime = 0;
+
+                TimeStopWatch.Restart();
+            } else {
+                Size = PrevSize;
+                SizeInBaseTime += addSize;
+            }
+        }
+
+        #endregion
+
+    }
+}
