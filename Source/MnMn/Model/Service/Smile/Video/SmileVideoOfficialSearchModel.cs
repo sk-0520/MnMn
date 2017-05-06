203803097bbe7cab6754ec4740821fcb62941038
sk <content.type.text.net@gmail.com>
2017-05-06 11:15:27 +0900

    URI定義が多分これでいいハズ。。。

--- Source/MnMn/Model/Service/Smile/Video/SmileVideoOfficialSearchModel.cs
+++ Source/MnMn/Model/Service/Smile/Video/SmileVideoOfficialSearchModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 using ContentTypeTextNet.Library.SharedLibrary.Model;
 
 namespace ContentTypeTextNet.MnMn.MnMn.Model.Service.Smile.Video
@@ -11,6 +12,19 @@ namespace ContentTypeTextNet.MnMn.MnMn.Model.Service.Smile.Video
     public class SmileVideoOfficialSearchModel: ModelBase
     {
         #region property
+
+        [XmlAttribute("max-index")]
+        public int MaximumIndex { get; set; }
+        [XmlAttribute("max-count")]
+        public int MaximumCount { get; set; }
+
+        [XmlArray("method"), XmlArrayItem("element")]
+        public CollectionModel<DefinedElementModel> Methods { get; set; } = new CollectionModel<DefinedElementModel>();
+        [XmlArray("sort"), XmlArrayItem("element")]
+        public CollectionModel<DefinedElementModel> Sort { get; set; } = new CollectionModel<DefinedElementModel>();
+        [XmlArray("type"), XmlArrayItem("element")]
+        public CollectionModel<DefinedElementModel> Type { get; set; } = new CollectionModel<DefinedElementModel>();
+
         #endregion
     }
 }
