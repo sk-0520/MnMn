06a8a88a412b909f0e53c71ee8527c2f2e122a91
sk <content.type.text.net@gmail.com>
2017-06-04 20:07:56 +0900

    ToSimpleSequence 追加

---
+++ Source/MnMn/Logic/Extensions/LinqExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContentTypeTextNet.MnMn.MnMn.Logic.Extensions
+{
+    public static class LinqExtensions
+    {
+        #region function
+
+        /// <summary>
+        /// <see cref="IEnumerable{T}.ToList"/>の代用。
+        /// <para>速度は求めてない、GCに関していい感じにできればそれでいい。</para>
+        /// <para>将来的に何かそれっぽいクラスをかませるかもしれない。</para>
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static IList<TSource> ToSimpleSequence<TSource>(this IEnumerable<TSource> source)
+        {
+            return source.ToList();
+        }
+
+        #endregion
+    }
+}
