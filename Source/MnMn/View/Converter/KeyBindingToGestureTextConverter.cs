d99076a0e22a705ebb46574f4350349d0003bfe4
sk <content.type.text.net@gmail.com>
2017-06-10 01:40:09 +0900

    試験的に「次の動画」キーバインドを共有してみた
    いけんじゃん

---
+++ Source/MnMn/View/Converter/KeyBindingToGestureTextConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Input;
+using ContentTypeTextNet.Library.SharedLibrary.Logic.Utility;
+
+namespace ContentTypeTextNet.MnMn.MnMn.View.Converter
+{
+    [ValueConversion(typeof(KeyBinding), typeof(string))]
+    public class KeyBindingToGestureTextConverter : IValueConverter
+    {
+        #region define
+
+        static KeyBinding DefaultKeyBinding { get; } = new KeyBinding();
+
+        #endregion
+
+        #region IValueConverter
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var keyBinding = CastUtility.GetCastWPFValue<KeyBinding>(value, null);
+            if(keyBinding == null) {
+                return value;
+            }
+
+            return $"{keyBinding.Modifiers} + {keyBinding.Key}";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+
+        #endregion
+    }
+}
