80432fae679fba0192848e364873eb11d15f46cc
sk <content.type.text.net@gmail.com>
2017-06-14 22:27:39 +0900

    resolved #626

--- Source/MnMn/Properties/AssemblyInfo.cs
+++ Source/MnMn/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 using System.Windows;
 using Meta.Vlc;
 
-[assembly: AssemblyVersion("0.68.12.*")]
+[assembly: AssemblyVersion("0.68.13.*")]
 
 #region あまり変わらない部分
 
