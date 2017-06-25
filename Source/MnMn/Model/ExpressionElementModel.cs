24634e873b985af11e1acad624ba8f4bb4a6db4e
sk <content.type.text.net@gmail.com>
2017-06-25 19:14:52 +0900

    IReadOnlyExpressionElement

--- Source/MnMn/Model/ExpressionElementModel.cs
+++ Source/MnMn/Model/ExpressionElementModel.cs
@@ -5,16 +5,18 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
 using ContentTypeTextNet.Library.SharedLibrary.Model;
+using ContentTypeTextNet.MnMn.MnMn.IF.ReadOnly;
 
 namespace ContentTypeTextNet.MnMn.MnMn.Model
 {
     [Serializable]
-    public class ExpressionElementModel: DefinedElementModel
+    public class ExpressionElementModel: DefinedElementModel, IReadOnlyExpressionElement
     {
         #region property
 
         [XmlElement("item")]
         public CollectionModel<ExpressionItemModel> Items { get; set; } = new CollectionModel<ExpressionItemModel>();
+        IReadOnlyList<IReadOnlyExpressionItem> IReadOnlyExpressionElement.Items => Items;
 
         #endregion
     }
