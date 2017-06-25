6f6bf8de9411f6f07f0b4edaf287228ef04007c3
sk <content.type.text.net@gmail.com>
2017-06-25 18:26:15 +0900

    IReadOnlyRaw

---
+++ Source/MnMn/IF/ReadOnly/IReadOnlyRaw.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContentTypeTextNet.MnMn.MnMn.IF.ReadOnly
+{
+    public interface IReadOnlyRaw
+    {
+        #region IReadOnlyRaw
+
+        /// <summary>
+        /// 生値。
+        /// </summary>
+        string Raw { get; }
+
+        #endregion
+    }
+}
