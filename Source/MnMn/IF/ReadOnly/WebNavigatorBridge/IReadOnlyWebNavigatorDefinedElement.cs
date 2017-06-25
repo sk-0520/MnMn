aca15ee9f9375183997d37856e7ef7b0c4aad27b
sk <content.type.text.net@gmail.com>
2017-06-25 21:53:54 +0900

    IReadOnlyWebNavigatorDefinedElement
    この名前空間は魑魅魍魎なんでやめときたい

---
+++ Source/MnMn/IF/ReadOnly/WebNavigatorBridge/IReadOnlyWebNavigatorDefinedElement.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ContentTypeTextNet.MnMn.Library.Bridging.Define;
+
+namespace ContentTypeTextNet.MnMn.MnMn.IF.ReadOnly.WebNavigatorBridge
+{
+    public interface IReadOnlyWebNavigatorDefinedElement: IReadOnlyDefinedElement
+    {
+        #region property
+
+        /// <summary>
+        /// 受け付けるサービス。
+        /// <para><see cref="ContentTypeTextNet.MnMn.MnMn.View.Controls.WebNavigator.ServiceType"/>が渡される。</para>
+        /// <para>基本的に<see cref="ServiceType.All"/>でOK。</para>
+        /// </summary>
+        ServiceType AllowService { get; }
+
+        /// <summary>
+        /// 処理するサービス。
+        /// <para><see cref="ServiceType.All"/>はダメ。</para>
+        /// </summary>
+        ServiceType SendService { get; }
+
+        #endregion
+    }
+}
