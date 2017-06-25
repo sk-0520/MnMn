a1ad9c660ec8eef78ef52564944cb1a71bd7d9ed
sk <content.type.text.net@gmail.com>
2017-06-25 23:22:38 +0900

    ランキングの定義を読み込み専用に変更

--- Source/MnMn/ViewModel/Controls/Service/Smile/Video/Ranking/SmileVideoRankingManagerViewModel.cs
+++ Source/MnMn/ViewModel/Controls/Service/Smile/Video/Ranking/SmileVideoRankingManagerViewModel.cs
@@ -37,8 +37,8 @@ namespace ContentTypeTextNet.MnMn.MnMn.ViewModel.Controls.Service.Smile.Video.Ra
     {
         #region variable
 
-        DefinedElementModel _selectedPeriod;
-        DefinedElementModel _selectedTarget;
+        IReadOnlyDefinedElement _selectedPeriod;
+        IReadOnlyDefinedElement _selectedTarget;
 
         SmileVideoRankingCategoryDefinedElementViewModel _selectedCategory;
 
@@ -64,7 +64,7 @@ namespace ContentTypeTextNet.MnMn.MnMn.ViewModel.Controls.Service.Smile.Video.Ra
         public IList<DefinedElementModel> PeriodItems { get { return RankingModel.Periods.Items; } }
         public IList<DefinedElementModel> TargetItems { get { return RankingModel.Targets.Items; } }
 
-        public DefinedElementModel SelectedPeriod
+        public IReadOnlyDefinedElement SelectedPeriod
         {
             get { return this._selectedPeriod; }
             set
@@ -74,7 +74,7 @@ namespace ContentTypeTextNet.MnMn.MnMn.ViewModel.Controls.Service.Smile.Video.Ra
                 }
             }
         }
-        public DefinedElementModel SelectedTarget
+        public IReadOnlyDefinedElement SelectedTarget
         {
             get { return this._selectedTarget; }
             set
@@ -195,7 +195,7 @@ namespace ContentTypeTextNet.MnMn.MnMn.ViewModel.Controls.Service.Smile.Video.Ra
             return LoadRankingCategoryCoreAsync(nowPeriod, nowTarget, nowCategory.Model);
         }
 
-        Task LoadRankingCategoryCoreAsync(DefinedElementModel period, DefinedElementModel target, IReadOnlyDefinedElement category)
+        Task LoadRankingCategoryCoreAsync(IReadOnlyDefinedElement period, IReadOnlyDefinedElement target, IReadOnlyDefinedElement category)
         {
             // 存在する場合は該当タブへ遷移
             var selectViewModel = RestrictUtility.IsNotNull(
