17b1327fabb67fd0bdd315b47fd2a4eff071aa36
sk <content.type.text.net@gmail.com>
2017-06-25 19:24:24 +0900

    IReadOnlyMappingItemNode

---
+++ Source/MnMn/IF/ReadOnly/IReadOnlyMappingItemNode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContentTypeTextNet.MnMn.MnMn.IF.ReadOnly
+{
+    interface IReadOnlyMappingItemNode
+    {
+        #region property
+
+        string Target { get; }
+
+        #endregion
+    }
+}
