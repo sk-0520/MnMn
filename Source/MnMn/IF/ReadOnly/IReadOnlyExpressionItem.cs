f51762d6ce757aaa9109777fc11141d053c29d09
sk <content.type.text.net@gmail.com>
2017-06-25 19:10:00 +0900

    IReadOnlyExpressionItem

---
+++ Source/MnMn/IF/ReadOnly/IReadOnlyExpressionItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using ContentTypeTextNet.MnMn.MnMn.Define;
+
+namespace ContentTypeTextNet.MnMn.MnMn.IF.ReadOnly
+{
+    public interface IReadOnlyExpressionItem
+    {
+        #region property
+
+        string Id { get; }
+
+        ExpressionItemKind Kind { get; }
+
+        RegexOptions RegexOptions { get; }
+
+        IReadOnlyCData Data { get; }
+
+        #endregion
+    }
+}
