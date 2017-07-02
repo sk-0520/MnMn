2e78f348d65c1bd021c9525b34f9b343f169c674
sk <content.type.text.net@gmail.com>
2017-07-02 20:24:11 +0900

    IReadOnlyThemeSetting

---
+++ Source/MnMn/IF/ReadOnly/Setting/IReadOnlyThemeSetting.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContentTypeTextNet.MnMn.MnMn.IF.ReadOnly.Setting
+{
+    public interface IReadOnlyThemeSetting
+    {
+        #region property
+
+        bool IsRandom { get; }
+
+        string ApplicationTheme { get; }
+
+        string BaseTheme { get; }
+
+        string Accent { get; }
+
+        #endregion
+    }
+}
