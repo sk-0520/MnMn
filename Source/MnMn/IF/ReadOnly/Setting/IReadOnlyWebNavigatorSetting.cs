4daa5883d09deb88ad406156a3a597246dde4e3a
sk <content.type.text.net@gmail.com>
2017-07-02 20:25:50 +0900

    IReadOnlyWebNavigatorSetting

---
+++ Source/MnMn/IF/ReadOnly/Setting/IReadOnlyWebNavigatorSetting.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContentTypeTextNet.MnMn.MnMn.IF.ReadOnly.Setting
+{
+    public interface IReadOnlyWebNavigatorSetting
+    {
+        #region property
+
+        /// <summary>
+        /// プラグインをシステムから読み込むか。
+        /// </summary>
+        bool GeckoFxScanPlugin { get; }
+
+        /// <summary>
+        /// ブラウザからのファイルダウンロード先。
+        /// </summary>
+        string DownloadDirectoryPath { get; }
+
+        #endregion
+    }
+}
