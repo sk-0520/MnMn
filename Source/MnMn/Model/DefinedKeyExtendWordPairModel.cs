4197a68ff8a084b26e61889893fd75b279aecb6f
sk <content.type.text.net@gmail.com>
2017-07-09 20:20:23 +0900

    #644 doc

---
+++ Source/MnMn/Model/DefinedKeyExtendWordPairModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ContentTypeTextNet.Library.SharedLibrary.IF;
+using ContentTypeTextNet.MnMn.MnMn.IF.ReadOnly;
+
+namespace ContentTypeTextNet.MnMn.MnMn.Model
+{
+    public class DefinedKeyExtendWordPairModel : KeyModelBase, IModel
+    {
+        public
+    }
+}
