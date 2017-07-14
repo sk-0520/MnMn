da9f5324ccb90a6bceb38ff76fef172bf7aedc1c
sk <content.type.text.net@gmail.com>
2017-07-14 22:15:11 +0900

    RawSmileVideoWatchDataEnvironmentModel

--- Source/MnMn/Model/Service/Smile/Video/Raw/RawSmileVideoWatchDataEnvironmentModel.cs
+++ Source/MnMn/Model/Service/Smile/Video/Raw/RawSmileVideoWatchDataEnvironmentModel.cs
@@ -13,6 +13,21 @@ namespace ContentTypeTextNet.MnMn.MnMn.Model.Service.Smile.Video.Raw
     {
         #region property
 
+        [DataMember(Name = "baseURL")]
+        public RawSmileVideoWatchDataEnvironmentBaseUrlModel BaseUrl { get; set; }
+
+        [DataMember(Name = "playlistToken")]
+        public string PlaylistToken { get; set; }
+
+        [DataMember(Name = "i18n")]
+        public RawSmileVideoWatchDataEnvironmentI18nModel I18n { get; set; }
+
+        [DataMember(Name = "urls")]
+        public RawSmileVideoWatchDataEnvironmentUrlsModel Urls { get; set; }
+
+        [DataMember(Name = "isMonitoringLogUser")]
+        public string IsMonitoringLogUser { get; set; }
+
         #endregion
     }
 }
---
+++ Source/MnMn/Model/Service/Smile/Video/Raw/RawSmileVideoWatchDataEnvironmentUrlsModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.Serialization;
+using ContentTypeTextNet.Library.SharedLibrary.Model;
+
+namespace ContentTypeTextNet.MnMn.MnMn.Model.Service.Smile.Video.Raw
+{
+    public class RawSmileVideoWatchDataEnvironmentUrlsModel : ModelBase
+    {
+        #region property
+
+        [DataMember(Name = "playerHelp")]
+        public string PlayerHelp { get;set;}
+
+        #endregion
+    }
+}
