38ff3f69407e035e78954974d2525b93be8e7592
sk <content.type.text.net@gmail.com>
2017-07-16 15:46:42 +0900

    新規分のデータ追加

---
+++ Source/MnMn/Model/Service/Smile/Video/Raw/RawSmileVideoMsgLeafModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using ContentTypeTextNet.Library.SharedLibrary.Model;
+
+namespace ContentTypeTextNet.MnMn.MnMn.Model.Service.Smile.Video.Raw
+{
+    [Serializable, DataContract]
+    public class RawSmileVideoMsgLeafModel : ModelBase
+    {
+        #region property
+
+        [DataMember(Name = "thread")]
+        public string Thread { get; set; }
+
+        [DataMember(Name = "leaf")]
+        public string Leaf { get; set; }
+
+        [DataMember(Name = "count")]
+        public string Count { get; set; }
+
+        #endregion
+    }
+}
