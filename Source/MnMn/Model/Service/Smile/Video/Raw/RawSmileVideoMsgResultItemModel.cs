32fc31233d02169a3283f20c1de65d2a2b3237a8
sk <content.type.text.net@gmail.com>
2017-07-16 23:43:12 +0900

    コメントデータを少し軽量化

--- Source/MnMn/Model/Service/Smile/Video/Raw/RawSmileVideoMsgResultItemModel.cs
+++ Source/MnMn/Model/Service/Smile/Video/Raw/RawSmileVideoMsgResultItemModel.cs
@@ -14,22 +14,22 @@ namespace ContentTypeTextNet.MnMn.MnMn.Model.Service.Smile.Video.Raw
     {
         #region property
 
-        [DataMember(Name = "ping")]
+        [DataMember(Name = "ping", EmitDefaultValue =false)]
         public RawSmileVideoMsgPingModel Ping { get; set; }
 
-        [DataMember(Name = "thread")]
+        [DataMember(Name = "thread", EmitDefaultValue = false)]
         public RawSmileVideoMsgThreadModel Thread { get; set; }
 
-        [DataMember(Name = "leaf")]
+        [DataMember(Name = "leaf", EmitDefaultValue = false)]
         public RawSmileVideoMsgLeafModel Leaf { get; set; }
 
-        [DataMember(Name = "global_num_res")]
+        [DataMember(Name = "global_num_res", EmitDefaultValue = false)]
         public RawSmileVideoMsgGlobalNumResModel GlobalNumRes { get; set; }
 
-        [DataMember(Name = "chat")]
+        [DataMember(Name = "chat", EmitDefaultValue = false)]
         public RawSmileVideoMsgChatModel Chat { get; set; }
 
-        [DataMember(Name = "chat_result")]
+        [DataMember(Name = "chat_result", EmitDefaultValue = false)]
         public RawSmileVideoMsgChatResultModel ChatResult { get; set; }
 
         #endregion
