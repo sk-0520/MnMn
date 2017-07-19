586caf3f3984aa835c611b781c7dbcfcbf10b5f5
sk <content.type.text.net@gmail.com>
2017-07-19 21:44:35 +0900

    必要データとしちゃこんなもんかな

---
+++ Source/MnMn/Logic/Service/Smile/Video/SmileVideoDmcLoader.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ContentTypeTextNet.Library.SharedLibrary.IF;
+using ContentTypeTextNet.Library.SharedLibrary.Logic;
+using ContentTypeTextNet.MnMn.MnMn.Model.Service.Smile.Video.Raw;
+
+namespace ContentTypeTextNet.MnMn.MnMn.Logic.Service.Smile.Video
+{
+    /// <summary>
+    /// DMC のゆりかごから墓場までを担当する。
+    /// </summary>
+    public class SmileVideoDmcLoader: DisposeFinalizeBase
+    {
+        public SmileVideoDmcLoader(string videoId, Uri uri, RawSmileVideoDmcObjectModel baseData, Mediation mediation)
+        {
+            VideoId = videoId;
+            Uri = uri;
+            BaseData = baseData;
+            Mediation = mediation;
+        }
+
+        #region property
+
+        public string VideoId { get; }
+
+        public Uri Uri { get; }
+
+        RawSmileVideoDmcObjectModel BaseData { get; }
+
+        Mediation Mediation { get; }
+
+        #endregion
+
+        #region function
+
+        #endregion
+    }
+}
