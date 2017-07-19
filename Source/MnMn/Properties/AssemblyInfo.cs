a70fd4dc8f131b47c90833f1c615ac2cec3b99df
sk <content.type.text.net@gmail.com>
2017-07-19 19:23:05 +0900

    #552 doc

--- Source/MnMn/Properties/AssemblyInfo.cs
+++ Source/MnMn/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 using System.Windows;
 using Meta.Vlc;
 
-[assembly: AssemblyVersion("0.73.7.*")]
+[assembly: AssemblyVersion("0.73.8.*")]
 
 #region あまり変わらない部分
 
