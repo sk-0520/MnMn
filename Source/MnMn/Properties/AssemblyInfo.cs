c79c3fa164846d217bb890544133827216b3c247
sk <content.type.text.net@gmail.com>
2017-07-22 00:40:38 +0900

    init 0.76.0

--- Source/MnMn/Properties/AssemblyInfo.cs
+++ Source/MnMn/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 using System.Windows;
 using Meta.Vlc;
 
-[assembly: AssemblyVersion("0.75.0.*")]
+[assembly: AssemblyVersion("0.75.1.*")]
 
 #region あまり変わらない部分
 
