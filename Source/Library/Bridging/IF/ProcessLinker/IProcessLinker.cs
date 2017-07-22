203d51f73b97b55e2fe19444a2a26754aa331f5c
sk <content.type.text.net@gmail.com>
2017-07-22 16:32:01 +0900

    とりあえず作ってみた

---
+++ Source/Library/Bridging/IF/ProcessLinker/IProcessLinker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using ContentTypeTextNet.MnMn.Library.Bridging.IF.ReadOnly;
+
+namespace ContentTypeTextNet.MnMn.Library.Bridging.IF.ProcessLink
+{
+    [ServiceContract(Namespace = "http://content-type-text.net/mnmn/ProcessLinker")]
+    public interface IProcessLinker
+    {
+        #region function
+
+        /// <summary>
+        /// ホストへ接続の挨拶を行う。
+        /// <para>WCFより上位で行われる約束事。</para>
+        /// </summary>
+        /// <param name="clientName">クライアント名称</param>
+        /// <returns>接続可能な場合セッション情報が返る。接続できない場合は null が返る。</returns>
+        [OperationContract]
+        IReadOnlyLinkSession Connect(string clientName);
+
+        #endregion
+    }
+}
---
+++ Source/Library/Bridging/Model/ProcessLinker/LinkSessionModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ContentTypeTextNet.Library.SharedLibrary.Model;
+using ContentTypeTextNet.MnMn.Library.Bridging.IF.ReadOnly;
+
+namespace ContentTypeTextNet.MnMn.Library.Bridging.Model.ProcessLinker
+{
+    [Serializable]
+    public class LinkSessionModel : ModelBase, IReadOnlyLinkSession
+    {
+        #region IReadOnlyLinkSession
+
+        public string ClientName { get; set; }
+
+        public string ClientId { get; set; }
+
+        #endregion
+    }
+}
