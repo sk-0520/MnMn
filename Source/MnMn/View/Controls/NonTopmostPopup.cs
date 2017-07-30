9a1f411c90d09c3d9f5408c40f584ebf9bb295d3
sk <content.type.text.net@gmail.com>
2017-07-30 18:57:34 +0900

    共通使用出来るとこは除外

--- Source/MnMn/View/Controls/NonTopmostPopup.cs
+++ Source/MnMn/View/Controls/NonTopmostPopup.cs
@@ -9,6 +9,8 @@ using System.Windows;
 using System.Windows.Controls.Primitives;
 using System.Windows.Input;
 using System.Windows.Interop;
+using ContentTypeTextNet.Library.PInvoke.Windows;
+using ContentTypeTextNet.Library.SharedLibrary.Logic.Utility;
 
 namespace ContentTypeTextNet.MnMn.MnMn.View.Controls
 {
@@ -21,7 +23,12 @@ namespace ContentTypeTextNet.MnMn.MnMn.View.Controls
         /// <summary>
         /// Is Topmost dependency property
         /// </summary>
-        public static readonly DependencyProperty IsTopmostProperty = DependencyProperty.Register("IsTopmost", typeof(bool), typeof(NonTopmostPopup), new FrameworkPropertyMetadata(false, OnIsTopmostChanged));
+        public static readonly DependencyProperty IsTopmostProperty = DependencyProperty.Register(
+            DependencyPropertyUtility.GetName(nameof(IsTopmostProperty)),
+            typeof(bool),
+            typeof(NonTopmostPopup),
+            new FrameworkPropertyMetadata(false, OnIsTopmostChanged)
+        );
 
         private bool? _appliedTopMost;
         private bool _alreadyLoaded;
@@ -139,75 +146,37 @@ namespace ContentTypeTextNet.MnMn.MnMn.View.Controls
 
             RECT rect;
 
-            if(!GetWindowRect(hwnd, out rect)) {
+            if(!NativeMethods.GetWindowRect(hwnd, out rect)) {
                 return;
             }
 
             Debug.WriteLine("setting z-order " + isTop);
 
             if(isTop) {
-                SetWindowPos(hwnd, HWND_TOPMOST, rect.Left, rect.Top, (int)Width, (int)Height, TOPMOST_FLAGS);
+                NativeMethods.SetWindowPos(hwnd, HWND_TOPMOST, rect.Left, rect.Top, (int)Width, (int)Height, TOPMOST_FLAGS );
             } else {
                 // Z-Order would only get refreshed/reflected if clicking the
                 // the titlebar (as opposed to other parts of the external
                 // window) unless I first set the popup to HWND_BOTTOM
                 // then HWND_TOP before HWND_NOTOPMOST
-                SetWindowPos(hwnd, HWND_BOTTOM, rect.Left, rect.Top, (int)Width, (int)Height, TOPMOST_FLAGS);
-                SetWindowPos(hwnd, HWND_TOP, rect.Left, rect.Top, (int)Width, (int)Height, TOPMOST_FLAGS);
-                SetWindowPos(hwnd, HWND_NOTOPMOST, rect.Left, rect.Top, (int)Width, (int)Height, TOPMOST_FLAGS);
+                NativeMethods.SetWindowPos(hwnd, HWND_BOTTOM, rect.Left, rect.Top, (int)Width, (int)Height, TOPMOST_FLAGS);
+                NativeMethods.SetWindowPos(hwnd, HWND_TOP, rect.Left, rect.Top, (int)Width, (int)Height, TOPMOST_FLAGS);
+                NativeMethods.SetWindowPos(hwnd, HWND_NOTOPMOST, rect.Left, rect.Top, (int)Width, (int)Height, TOPMOST_FLAGS);
             }
 
             this._appliedTopMost = isTop;
         }
 
         #region P/Invoke imports & definitions
-#pragma warning disable 1591 //Xml-doc
-#pragma warning disable 169 //Never used-warning
-        // ReSharper disable InconsistentNaming
-        // Imports etc. with their naming rules
-
-        [StructLayout(LayoutKind.Sequential)]
-        public struct RECT
-
-        {
-            public int Left;
-            public int Top;
-            public int Right;
-            public int Bottom;
-        }
-
-        [DllImport("user32.dll")]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        private static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
-
-        [DllImport("user32.dll")]
-        private static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X,
-        int Y, int cx, int cy, uint uFlags);
 
         static readonly IntPtr HWND_TOPMOST = new IntPtr(-1);
         static readonly IntPtr HWND_NOTOPMOST = new IntPtr(-2);
         static readonly IntPtr HWND_TOP = new IntPtr(0);
         static readonly IntPtr HWND_BOTTOM = new IntPtr(1);
 
-        private const UInt32 SWP_NOSIZE = 0x0001;
-        const UInt32 SWP_NOMOVE = 0x0002;
-        const UInt32 SWP_NOZORDER = 0x0004;
-        const UInt32 SWP_NOREDRAW = 0x0008;
-        const UInt32 SWP_NOACTIVATE = 0x0010;
+        const SWP TOPMOST_FLAGS =
+            SWP.SWP_NOACTIVATE | SWP.SWP_NOOWNERZORDER | SWP.SWP_NOSIZE | SWP.SWP_NOMOVE | SWP.SWP_NOREDRAW | SWP.SWP_NOSENDCHANGING;
 
-        const UInt32 SWP_FRAMECHANGED = 0x0020; /* The frame changed: send WM_NCCALCSIZE */
-        const UInt32 SWP_SHOWWINDOW = 0x0040;
-        const UInt32 SWP_HIDEWINDOW = 0x0080;
-        const UInt32 SWP_NOCOPYBITS = 0x0100;
-        const UInt32 SWP_NOOWNERZORDER = 0x0200; /* Don’t do owner Z ordering */
-        const UInt32 SWP_NOSENDCHANGING = 0x0400; /* Don’t send WM_WINDOWPOSCHANGING */
-
-        const UInt32 TOPMOST_FLAGS =
-            SWP_NOACTIVATE | SWP_NOOWNERZORDER | SWP_NOSIZE | SWP_NOMOVE | SWP_NOREDRAW | SWP_NOSENDCHANGING;
-
-        // ReSharper restore InconsistentNaming
-#pragma warning restore 1591
-#pragma warning restore 169
         #endregion
     }
 }
