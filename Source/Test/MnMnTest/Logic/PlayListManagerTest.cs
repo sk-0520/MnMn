34655a6e6208c490a435d5f421edda8f71a39ef1
sk <content.type.text.net@gmail.com>
2017-08-01 22:03:53 +0900

    クラッシュレポート記載情報の再現OK

--- Source/Test/MnMnTest/Logic/PlayListManagerTest.cs
+++ Source/Test/MnMnTest/Logic/PlayListManagerTest.cs
@@ -149,5 +149,62 @@ namespace MnMnTest.Logic
             Assert.IsTrue(list.ChangeNextItem() == selectItem);
             Assert.IsTrue(list.ChangeNextItem() == nextItem);
         }
+
+        [TestMethod]
+        public void Issue705ReproduceTest()
+        {
+            {
+                var list = new PlayListManager<Item>();
+                list.AddRange(Enumerable.Range(1, 10).Select(i => new Item(i)));
+
+                var num1 = list.CurrenItem;
+                var num2 = list.ChangeNextItem();
+                var num3 = list.ChangeNextItem();
+                var num4 = list.ChangeNextItem();
+
+                // ここで古いのを破棄
+                list.Remove(num1);
+
+                var num5 = list.ChangeNextItem();
+                Assert.IsTrue(num5.Number == 6);
+            }
+            {
+                // 消した分だけずれる試験
+                var list = new PlayListManager<Item>();
+                list.AddRange(Enumerable.Range(1, 10).Select(i => new Item(i)));
+
+                var num1 = list.CurrenItem;
+                var num2 = list.ChangeNextItem();
+                var num3 = list.ChangeNextItem();
+                var num4 = list.ChangeNextItem();
+
+                list.Remove(num1);
+                list.Remove(num2);
+                list.Remove(num3);
+
+                var num5 = list.ChangeNextItem();
+                Assert.IsTrue(num5.Number == 8);
+            }
+            {
+                // 落ちるパターン
+                var list = new PlayListManager<Item>();
+                list.AddRange(Enumerable.Range(1, 4).Select(i => new Item(i)));
+
+                var num1 = list.CurrenItem;
+                var num2 = list.ChangeNextItem();
+                var num3 = list.ChangeNextItem();
+
+                list.Remove(num1);
+                list.Remove(num2);
+                list.Remove(num3);
+
+                try {
+                    var num4 = list.ChangeNextItem();
+                    Assert.Fail();
+                } catch {
+                    Assert.IsTrue(true);
+                }
+            }
+        }
     }
 }
