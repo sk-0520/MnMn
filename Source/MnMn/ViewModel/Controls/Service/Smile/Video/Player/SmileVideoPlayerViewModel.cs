cb57e5f96d5751784f04251a8d481d563553f953
sk <content.type.text.net@gmail.com>
2017-08-06 05:43:26 +0900

    実際の再生終了後挙動に設定を反映

--- Source/MnMn/ViewModel/Controls/Service/Smile/Video/Player/SmileVideoPlayerViewModel.cs
+++ Source/MnMn/ViewModel/Controls/Service/Smile/Video/Player/SmileVideoPlayerViewModel.cs
@@ -1623,20 +1623,27 @@ namespace ContentTypeTextNet.MnMn.MnMn.ViewModel.Controls.Service.Smile.Video.Pl
                 return;
             }
 
+            var skipReplayProcess = false;
             if(CanPlayNextVideo.Value && PlayListItems.Skip(1).Any()) {
-                // 次のプレイリストへ遷移
-                Mediator.Logger.Debug("next playlist item");
-                LoadNextPlayListItemAsync();
-                return;
+                if(LastPlayListItemIsStop && PlayListItems.IsLastItem) {
+                    skipReplayProcess = true;
+                } else {
+                    // 次のプレイリストへ遷移
+                    Mediator.Logger.Debug("next playlist item");
+                    LoadNextPlayListItemAsync();
+                    return;
+                }
             }
 
-            if(ReplayVideo) {
-                // リプレイ
-                Mediator.Logger.Debug("replay");
-                StopMovie(true);
-                PlayMovie();
+            if(!skipReplayProcess) {
+                if(ReplayVideo) {
+                    // リプレイ
+                    Mediator.Logger.Debug("replay");
+                    StopMovie(true);
+                    PlayMovie();
 
-                return;
+                    return;
+                }
             }
 
             // 普通の停止
