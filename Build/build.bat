32bf0ac5938692674bb4143924e8dee3872e4884
sk <content.type.text.net@gmail.com>
2017-08-14 22:05:34 +0900

    これかよ！

--- Build/build.bat
+++ Build/build.bat
@@ -26,7 +26,7 @@ rem )
 set MB=%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe
 
 echo build x86
-"%MB%" Source\MnMn.sln /p:DefineConstants="BUILD;%1" /p:Configuration=Release;Platform=x86 /t:Rebuild /m /p:TargetFrameworkVersion=v4.6
+"%MB%" Source\MnMn.sln /p:DefineConstants="BUILD;%1" /p:Configuration=Release;Platform=x86 /t:Rebuild /m /p:TargetFrameworkVersion=v4.7
 set ERROR_X86=%ERRORLEVEL%
 
 rem echo build x64
